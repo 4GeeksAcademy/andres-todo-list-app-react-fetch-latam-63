963f7d91f69fe593c8e0836db9277ce6a1f1e3ff
AEGiner <andresesquivel2011@gmail.com>
2025-11-12 23:31:23 +0000

    changed font

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import List from "./List";
 
-
 //create your first component
 const Home = () => {
-	return (
-		<div className="container">
-			<div className="d-flex justify-content-center row mt-5">
-				<h1 className="title col-12 text-center">todos</h1>
-				<List />
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-center row mt-5">
+        <h1 className="title col-12 text-center">todos</h1>
+        <List />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/js/components/List.jsx
+++ src/js/components/List.jsx
@@ -1,65 +1,92 @@
 import React, { useState } from "react";
 
-
 const List = () => {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [isVisible, setIsVisible] = useState(null);
 
-    const [tasks, setTasks] = useState([]);
-    const [inputValue, setInputValue] = useState("");
-    const [isVisible, setIsVisible] = useState(null)
+  function sendData(e) {
+    e.preventDefault();
+  }
 
-    const CreateTask = (event) => {
-        if (event.key === 'Enter' && inputValue !== '') {
-            setTasks([...tasks, inputValue]); setInputValue('')
-        }
-    };
-
-    const DeleteTask = (index) => {
-        const newTasks = tasks.filter((item, currentIndex) => currentIndex !== index);
-        setTasks(newTasks)
-    };
-
-    const Capitalize = (item) => {
-        return item.charAt(0).toUpperCase() + item.slice(1);
-    };
-
-    const TaskCounter = (array) => {
-        if (array.length === 1) {
-            return `${array.length} Item left`
-        } return `${array.length} Items left`
-    };
-
-    const MouseEnter = (event) => {
-        setIsVisible(event.target.id);
-    };
-
-    const MouseLeave = () => {
-        setIsVisible(null);
-    };
-
-    function Visibility(item) {
-        if (isVisible == item) {
-            return 'visible'
-        } return 'hidden'
-    };
-
-    const Placeholder = (array) => {
-        if (array.length === 0) {
-            return 'No tasks, add a task'
-        } return 'What needs to be done?'
+  const CreateTask = (event) => {
+    if (event.key === "Enter" && inputValue !== "") {
+      setTasks([...tasks, inputValue]);
+      setInputValue("");
     }
+  };
 
-    return (
+  const DeleteTask = (index) => {
+    const newTasks = tasks.filter(
+      (item, currentIndex) => currentIndex !== index
+    );
+    setTasks(newTasks);
+  };
 
-        <ul className="todo-list list-group col-6 list-group-flush">
-            <li className="list-group-item ps-5"><input type="text" className="input-field" placeholder={Placeholder(tasks)} onChange={e => setInputValue(e.target.value)} value={inputValue} onKeyDown={CreateTask} /></li>
-            {tasks.map((item, index) => {
-                return (
-                    <li className="list-group-item ps-5 list-item" key={index} id={index} onMouseEnter={MouseEnter} onMouseLeave={MouseLeave}>{Capitalize(item)}
-                        <i className="fa-solid fa-x delete-marker" key={index} style={{ visibility: `${Visibility(index)}` }} onClick={() => DeleteTask(index)}></i></li>
-                )
-            })}
-            <li className="task-counter list-group-item ps-3">{TaskCounter(tasks)}</li>
-        </ul>
-    )
+  const Capitalize = (item) => {
+    return item.charAt(0).toUpperCase() + item.slice(1);
+  };
+
+  const TaskCounter = (array) => {
+    if (array.length === 1) {
+      return `${array.length} Item left`;
+    }
+    return `${array.length} Items left`;
+  };
+
+  function Visibility(index) {
+    if (isVisible == index) {
+      return "visible";
+    }
+    return "hidden";
+  }
+
+  const Placeholder = (array) => {
+    if (array.length === 0) {
+      return "No tasks, add a task";
+    }
+    return "What needs to be done?";
+  };
+
+  return (
+    <form onSubmit={sendData} className="d-flex justify-content-center">
+      <ul className="todo-list list-group col-5 list-group-flush">
+        <li className="list-group-item ps-5">
+          <input
+            type="text"
+            className="input-field form-control"
+            placeholder={Placeholder(tasks)}
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={CreateTask}
+          />
+        </li>
+        {tasks.map((task, index) => {
+          return (
+            <li
+              className="list-group-item ps-5 list-item"
+              key={index}
+              id={index}
+              onMouseEnter={(e) => setIsVisible(e.target.id)}
+              onMouseLeave={() => setIsVisible(null)}
+            >
+              {Capitalize(task)}
+              <i
+                className="delete-marker"
+                key={index}
+                style={{ visibility: `${Visibility(index)}` }}
+                onClick={() => DeleteTask(index)}
+              >
+                X
+              </i>
+            </li>
+          );
+        })}
+        <li className="task-counter list-group-item ps-3">
+          {TaskCounter(tasks)}
+        </li>
+      </ul>
+    </form>
+  );
 };
 export default List;
